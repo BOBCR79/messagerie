1c8588eeb88db09a01ff4723a2fc7f0b0b08e44e
BoubacarFaye <146725708+BoubacarFaye@users.noreply.github.com>
2024-07-03 18:35:21 +0200

    Create CommentType.php
    créé CommentType & ajout dans controller

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\CommentsRepository;
+use App\Entity\Comments;
+use App\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,12 +18,26 @@ class CommentController extends AbstractController
     #[Route('/comment/post/{postId}', name: 'app_comment')]
     public function index(int $postId): Response
     {
-        $commentsList = $this->CommentsRepository->findBy(['post_id' => $postId]);
+
+        //TODO verif si user connecte, si non, masquer inputs
+
+        $commentsList = $this->CommentsRepository->fetchComments($postId);
+
+        $comment = new Comments();
+        $form = $this->createForm(CommentType::class, $comment);
+
+
+        $form->handleRequest($request);
+        if ($form->isSubmited() && $form->isValid())
+        {
+           //TODO insertion, persist & flush
+        }
 
         return $this->render('comment/index.html.twig', [
             'controller_name' => 'CommentController',
-            'comments' => commentsList,
-            'parentPost' => $postId
+            'comments' => $commentsList,
+            'parentPost' => $postId,
+            'comment_form' => $form
         ]);
     }
     
