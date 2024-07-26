1a13b512ff3499a47263c9b08c638bc6c6af7ba7
BoubacarFaye <146725708+BoubacarFaye@users.noreply.github.com>
2024-07-26 10:22:40 +0200

    ADD: user page
    empty

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,16 +1,17 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[Route('/user/{userHandle}', name: 'app_user')]
     public function index(): Response
     {
+        
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
