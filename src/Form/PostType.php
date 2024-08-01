9c7af290c9e823c4969b9cbf6802475b58a2f289
BoubacarFaye <146725708+BoubacarFaye@users.noreply.github.com>
2024-08-01 17:46:01 +0200

    Update PostType.php

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -3,27 +3,37 @@
 namespace App\Form;
 
 use App\Entity\Post;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use symfony\component\validator\Constraints\Image;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
 
 class PostType extends AbstractType
 {
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content')
-            ->add('media')
-            ->add('posted_at', null, [
-                'widget' => 'single_text',
+            ->add('content', TextareaType::class,[
+                'label' => $this->translator->trans("new_post"),
+                'attr' =>['class'=>'flex flex-col gap-4 w-96']
             ])
-            ->add('user_id', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
+            ->add('image', FileType::class, [
+                'required' => false,
+                'mapped' => false,
+//                'constraints' => new Image( maxSize: '2048k')
             ])
-        ;
+            ->add('Envoyer', SubmitType::class, [
+                'attr' => ['class'=> 'bg-green-800 text-white font-bold p-2 rounded-lg mt-2']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
