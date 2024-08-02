2b0216909970e0dc33dc734c74721c89249a41ce
BoubacarFaye <146725708+BoubacarFaye@users.noreply.github.com>
2024-08-02 16:55:41 +0200

    add: pagination session

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,25 +3,32 @@
 namespace App\Controller;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Attribute\Route;
-use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
 
     private $postList;
 
+
     public function __construct(private PostRepository $PostRepository)
     {
 
     }
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $postList = $this->PostRepository->fetchPostsWithUsername();
+
+        $session = $request->getSession();
+        $session->has('perPage')? $perPage = $session->get('perPage') : $perPage = 20;
+
+        $postList = $this->PostRepository->fetchPaginatedPosts(0,$perPage);
         return $this->render('home/index.html.twig', [
             'messages' => $postList
+
         ]);
     }
 }
